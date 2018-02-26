f74b2a5042ca2e82ea25c1e8b3ac08f08592af7f
Karl McCollester <karlmccollester@udhaa.com>
2018-02-26 10:45:40 -0500

    DataSource validations

--- app/models/data_source.rb
+++ app/models/data_source.rb
@@ -1,3 +1,6 @@
 class DataSource < ApplicationRecord
+  REFRESH_FREQUENCIES = ['Real-Time', 'Daily','Weekly','Monthly', 'Other (See Notes)', 'Not Updated']
   has_many :data_sets, dependent: :destroy
+  validates :name, uniqueness: { case_sensitive: false }, presence: true
+  validates_inclusion_of :refresh_frequency, :in => REFRESH_FREQUENCIES
 end
