5f7b5fec18219ba3b95f5013e76285a627ebb6e9
Karl McCollester <karlmccollester@udhaa.com>
2018-02-26 10:21:27 -0500

    add model relationships

--- app/models/data_set.rb
+++ app/models/data_set.rb
@@ -1,3 +1,6 @@
 class DataSet < ApplicationRecord
   belongs_to :data_source
+  has_many :data_set_archetypes
+  has_many :data_archetypes, through: :data_set_archetypes
+  has_many :data_elements
 end
