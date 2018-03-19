bef75d74638fcdcbe699805db14bd391a3468846
Karl McCollester <karlmccollester@udhaa.com>
2018-03-19 11:56:06 -0400

    add DataArchetype#add_default_color and after_create_commit callback

--- app/models/data_archetype.rb
+++ app/models/data_archetype.rb
@@ -1,4 +1,13 @@
 class DataArchetype < ApplicationRecord
   has_many :data_set_archetypes, dependent: :destroy
   has_many :data_sets, through: :data_set_archetypes
+  after_create_commit :add_default_color
+
+# private
+  def add_default_color
+    if color_swatch.nil?
+      cp = ColorPicker.new(instance: id)
+      update_attribute(:color_swatch, cp.color_swatch)
+    end
+  end
 end
